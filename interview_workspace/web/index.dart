60c6881c53edc897e746f84426e711d9306701dc
Aaron J Rubin <aaronjrubin1@gmail.com>
2015-03-19 16:23:45 +0900

    Add labels

--- interview_workspace/web/index.dart
+++ interview_workspace/web/index.dart
@@ -14,9 +14,12 @@ void addCategory() {
    categories.appendHtml('''<li>
       <div class="category">
         <button class="delete-button">削除</button>
-        <input type="text" class="fragment">
-        <input type="text" class="question">
-        <input type="checkbox" class="possessive">
+        <label>文型（{}で下線部を指定して）
+        <input type="text" class="fragment"></label>
+        <label>質問の仕方
+        <input type="text" class="question"></label>
+        <label>Iの代わりにMyで始まるか
+        <input type="checkbox" class="possessive"></label>
         <ol class="items">
           <li><input type="text" class="item"></li>
           <li><input type="text" class="item"></li>
